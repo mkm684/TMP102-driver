48fea5de4603b56dfc1f047841d9858a9e044184
mahmoud <Mohamed.Mahmoud@mss.ca>
2020-09-28 16:00:30 -0400

    driver initial version 1.1 (restruct)

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-avr-gcc -Os -DF_CPU=8000000 -mmcu=atmega328p -c main.c uart.c i2c_twi.c
-avr-gcc -DF_CPU=8000000 -mmcu=atmega328p -o main.elf main.o uart.o i2c_twi.o
-avr-objcopy -O ihex main.elf main.hex
-avrdude -c arduino -p atmega328p -P COM19 -U flash:w:main.hex
+avr-gcc -Os -DF_CPU=8000000 -mmcu=atmega328p -c src/main.c src/uart.c src/i2c_twi.c
+avr-gcc -DF_CPU=8000000 -mmcu=atmega328p -o bin/main.elf bin/main.o bin/uart.o bin/i2c_twi.o
+avr-objcopy -O ihex bin/main.elf bin/main.hex
+avrdude -c arduino -p atmega328p -P COM19 -U flash:w:bin/main.hex
